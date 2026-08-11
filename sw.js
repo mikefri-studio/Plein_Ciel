a448173473ff08b259164cd608fbd8117e6909fc
Micfri <44413144+mikefri@users.noreply.github.com>
2026-08-11 17:09:28 +0000

    Version 1.5.2 : toast au-dessus des modales et remonté

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pleinciel-1.5.1';
+const CACHE = 'pleinciel-1.5.2';
 const CORE = [
   './', 'index.html', 'css/style.css',
   'js/core.js','js/api.js','js/alerts.js','js/info.js','js/info-versions.js',
