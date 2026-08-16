3fb5d7e74ead4a5cee4210fc5af6dde6ca9f2e92
mikefri-studio <mikefri.app@gmail.com>
2026-08-16 17:13:31 +0000

    Revert "🌊 v1.7.3 : réglage infos marine détaillées (future feature PRO)"
    This reverts commit 9db409378e54704b99c805d26f3560843606ee2e.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pleinciel-1.7.3';
+const CACHE = 'pleinciel-1.7.2';
 const CORE = [
   './', 'index.html', 'css/style.css',
   'js/core.js','js/api.js','js/alerts.js','js/info.js','js/info-versions.js',
