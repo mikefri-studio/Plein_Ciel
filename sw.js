28c3806467ff3354709ff94a4a9875a5f11e3b9a
mikefri-studio <mikefri.app@gmail.com>
2026-08-16 18:14:49 +0000

    🧹 Historique : retrait des entrées PRO

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pleinciel-1.7.6';
+const CACHE = 'pleinciel-1.7.7';
 const CORE = [
   './', 'index.html', 'css/style.css',
   'js/core.js','js/api.js','js/alerts.js','js/info.js','js/info-versions.js',
