846b9c52b68d36d7edb16bb678b66a0cc2483e39
mikefri-studio <mikefri.app@gmail.com>
2026-08-16 14:12:53 +0000

    🔖 v1.7.0 : numérotation cohérente (cycle 1.7 fusionné)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pleinciel-1.8.0';
+const CACHE = 'pleinciel-1.7.0';
 const CORE = [
   './', 'index.html', 'css/style.css',
   'js/core.js','js/api.js','js/alerts.js','js/info.js','js/info-versions.js',
